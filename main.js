9b4144789e91892e3eaec996172755f9e4d9bd2f
DavidWei95036593 <dw36593@pausd.us>
2017-11-13 19:28:37 -0800

    finish question 1 and 2

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("hi");
+let box1 = document.querySelector("#question1");
+//Question 1
+box1.addEventListener("click",function(){
+	if (box1.classList.contains("move-down")){
+		box1.classList.remove("move-down");
+	}else{box1.classList.add("move-down");}
+	
+})
+
+//Question 2
+let box2 = document.querySelector("#question2")
+box2.addEventListener("click",function(){
+	if (box2.classList.contains("move-diaright")){
+		box2.classList.remove("move-diaright");
+	}else{box2.classList.add("move-diaright");}
+	if (box2.classList.contains("red")){
+		box2.classList.remove("red");
+		box2.classList.add("blue");}
+		else{
+			box2.classList.remove("blue");
+			box2.classList.add("red")}
+	
+})
