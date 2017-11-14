d2d833c19bc1bb017ac58788e4a5ad68a749cbde
DavidWei95036593 <dw36593@pausd.us>
2017-11-14 14:31:35 -0800

    fix question 2

--- main.js
+++ main.js
@@ -8,22 +8,25 @@ box1.addEventListener("click",function(){
 	if (box1.classList.contains("move-down")){
 		box1.classList.remove("move-down");
 	}else{box1.classList.add("move-down");}
-	
+
 })
 
 //Question 2
 let box2 = document.querySelector("#question2");
 box2.addEventListener("click",function(){
 	if (box2.classList.contains("move-diaright")){
+
 		box2.classList.remove("move-diaright");
-	}else{box2.classList.add("move-diaright");}
-	if (box2.classList.contains("red")){
-		box2.classList.remove("red");
-		box2.classList.add("blue");}
-		else{
-			box2.classList.remove("blue");
-			box2.classList.add("red")}
-	
+
+	}else{box2.classList.add("move-diaright");
+	}
+	// if (box2.classList.contains("red")){
+	// 	box2.classList.remove("red");
+	// 	box2.classList.add("blue");}
+	// 	else{
+	// 		box2.classList.remove("blue");
+	// 		box2.classList.add("red")}
+
 })
 //Question 4
 let box3 = document.querySelector("#box3");
@@ -34,8 +37,10 @@ input1.addEventListener("click",function(event){
 	}
 		else{
 		box3.classList.add("move-once");
+		// box3.classList.remove("move-once");
+
 	}
-	
+
 })
 
 
@@ -46,5 +51,5 @@ input.addEventListener("keydown", function(event){
 	if(event.key ==="ArrowDown"){
 		console.log("You've pressed the down key!")
 	}
-	
+
 });
